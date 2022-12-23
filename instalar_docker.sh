a138e84fdf9118ab8acccca4db384f5faeb62ec2
F3RR4LL <thiagodevmobile@gmail.com>
2022-12-22 20:17:04 -0400

    upload
    subindo os arquivos

---
+++ instalar_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Autor Thiago Araújo
+#GitHub: https://github.com/mathfirewall
+
+curl -fsSL https://get.docker.com | sudo bash
+sudo curl -fsSL "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker vagrant
---
+++ master.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Autor Thiago Araújo
+#GitHub: https://github.com/mathfirewall
+
+sudo su
+sudo docker swarm init --advertise-addr=10.10.10.100
+sudo docker swarm join-token worker | grep docker >> /vagrant/worker.sh
+sudo echo "sudo apt install nfs-common -y" >> /vagrant/worker.sh
+sudo echo "sleep 100" >> /vagrant/worker.sh
+sudo echo "sudo mount 10.10.10.100:/var/lib/docker/volumes/app/_data /var/lib/docker/volumes/app/_data" >> /vagrant/worker.sh
+
+sudo docker volume create app
+cd /var/lib/docker/volumes/app/_data
+sudo docker volume create data
+
+sudo docker run -e MYSQL_ROOT_PASSWORD=Senha123 -e MYSQL_DATABASE=meubanco --name mysql_server -d -p 3306:3306 --mount type=volume,src=data,dst=/var/lib/mysql mysql:5.7
+sudo docker service create --name app_web --replicas 15 -dt -p 80:80 --mount type=volume,src=app,dst=/app/ webdevops/php-apache:alpine-php7
+
+sudo apt install nfs-server -y
+
+sudo echo "/var/lib/docker/volumes/app/_data *(rw,sync,subtree_check)" >> /etc/exports
+sudo exportfs -ar
+
+#clonando a pasta do repositório
+sudo git clone https://github.com/mathfirewall/Login_Cadastro_PHP.git
+#copiando os arquivos para a pasta _data 
+sudo cp -R Login_Cadastro_PHP/* /var/lib/docker/volumes/app/_data
